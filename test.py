96cfb9ef72a201deec13c6211f2fa37189c94ba9
Keshav <keshavjha018@gmail.com>
2021-06-13 20:15:24 +0530

    added test file

---
+++ test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+PATH = 'C:\Program Files (x86)\chromedriver.exe'
+driver = webdriver.Chrome(PATH)
+import time 
+from selenium.webdriver.common.keys import Keys
+
+driver.get("https://getintopc.com")
+
+searchbox = driver.find_element_by_xpath('//*[@id="instance-atom-search-3"]/div/form/fieldset/input[1]')
+searchbox.send_keys("Windows 10")
+searchbox.send_keys(Keys.RETURN)
